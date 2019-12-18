b6f2447e8015bdd5000a82cb185ee4eff2ab3b10
Unknows <firstpan555@gmail.com>
2019-12-18 17:16:25 +0700

    [ADD]main component

---
+++ src/components/Core/Index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import Major from '../Major/Index'
+
+export default class componentName extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Major />
+      </React.Fragment>
+    )
+  }
+}
