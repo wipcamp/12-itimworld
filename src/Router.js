cd8b433ad2b9cd26f6c905c64378736c4df9801e
Unknows <firstpan555@gmail.com>
2019-12-20 16:37:52 +0700

    [UPDATE]add Login path and change root path

--- src/Router.js
+++ src/Router.js
@@ -6,6 +6,7 @@ import {
   Link,
 } from 'react-router-dom'
 
+import Login from './components/Login'
 import Profile from './components/Profile'
 import Major from './components/Major'
 import Questions from "./components/Questions";
@@ -15,11 +16,12 @@ export default class Index extends React.Component {
   render() {
     return (
       <Router>
-        <Link to="/" >1</Link>
+        <Link to="/profile" >1</Link>
         <Link to="/major" >2</Link>
         <Link to="/questions" >3</Link>
         <Switch>
-          <Route exact path="/" component={Profile} />
+          <Route exact path="/" component={Login} />
+          <Route exact path="/profile" component={Profile} />
           <Route exact path="/major" component={Major} />
           <Route exact path="/questions" component={Questions} />
         </Switch>
