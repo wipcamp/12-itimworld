310322101778258c3734a83bc91a2a58f90b1513
Unknown <mint29006@gmail.com>
2019-12-20 16:05:02 +0700

    Auto stash before merge of "master" and "origin/master"

--- src/components/Questions/Index.js
+++ src/components/Questions/Index.js
@@ -3,24 +3,19 @@ import ButtonRoute from '../Core/ButtonRoute'
 import Question from './Question'
 
 const arr = [
-    {questionId:1,ans:'jkjdklsjd'},
-    {questionId:2,ans:'fsd'},
-    {questionId:3,ans:'dsds'},
+	{questionId:1,ans:''},
+	{questionId:2,ans:''},
+	{questionId:3,ans:''},
 ];
-
 export default class Index extends Component {
     render() {
         return (
             <React.Fragment>
                 <div>
-                    <form>
                         {arr.map((data,i) => {
                             return <Question key={i}  questionId={data.questionId}  ans={data.ans}/>
                         })}
-                        <Question/>
-                    </form>
                 </div>
-
                 <ButtonRoute bgColor="pink" buttonRight="back" buttonLeft="Submit" />
             </React.Fragment>
         )
