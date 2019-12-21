b770f82ef871538a69fa57137e02e6e0c9f85f2b
Unknown <mint29006@gmail.com>
2019-12-21 16:09:08 +0700

    [UPDATE] move function in questions component

--- src/components/Questions/Question.js
+++ src/components/Questions/Question.js
@@ -4,41 +4,14 @@ import styled from 'styled-components'
 const Pink = styled.p`
     color: pink;
 `
-
-const answer = [];
-
 export default class Question extends Component {
-
-    handleAnswer = event => {
-      const val = event.target.value;
-      // this.setState({
-      //   question_id: this.props.questionId,
-      //   [nam]: val
-      // });
-      //console.log(this.state);
-      let doneEdit = false;
-      
-      for(var i = 0;i<answer.length;i++){
-        if(answer[i].question_id === this.props.questionId){
-          answer[i].answer_content = val;
-          doneEdit = true;
-        }
-      }
-      if(!doneEdit){
-        answer.push({
-          "question_id":this.props.questionId,
-          "answer_content":val
-        })
-      }
-      console.log(answer);
-    };
 		
     render() {
         return (
             <div>
             <Pink>Question {this.props.questionId}</Pink>
-            <textarea onChange={this.handleAnswer} row="100" cols="100"></textarea>
-            </div>	
+            <textarea name={this.props.questionId} onChange={this.props.handleAnswer} row="100" cols="100"></textarea>
+            </div>
         )
     }
 }
--- src/components/Questions/index.js
+++ src/components/Questions/index.js
@@ -3,17 +3,41 @@ import ButtonRoute from '../Core/ButtonRoute'
 import Question from './Question'
 
 const arr = [
-	{questionId:1,ans:''},
-	{questionId:2,ans:''},
-	{questionId:3,ans:''},
+	{questionId:1,question_content:''},
+	{questionId:2,question_content:''},
+	{questionId:3,question_content:''}
 ];
+
+const answer = [];
 export default class Index extends Component {
+
+  handleAnswer=(event)=> {
+    const val = event.target.value;
+    let doneEdit = false;
+    
+    for(var i = 0;i<answer.length;i++){
+      if(answer[i].question_id == event.target.name){
+        answer[i].answer_content = val;
+        doneEdit = true;
+      }
+
+    }
+    if(!doneEdit){
+      answer.push({
+        "question_id":1,
+        "answer_content":val
+      })
+    }
+
+    console.log(answer);
+  };
+
     render(){
         return (
             <React.Fragment>
                 <div>
                         {arr.map((data,i) => {
-                            return <Question questionCount={i+1}  questionId={data.questionId}  ans={data.ans}/>
+                            return <Question questionCount={i+1}  questionId={data.questionId} handleAnswer={this.handleAnswer}/>
                         })}
                 </div>
                 <ButtonRoute 
