cdf7fa67bb8ccf2f64208935f046fa0be3a404ee
Nope <o_o.toto@windowslive.com>
2019-12-22 19:04:50 +0700

    [ADD] add component Preview/index.js

---
+++ src/components/Preview/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Index extends Component {
+
+  render() {
+    return (
+      <React.Fragment>
+        This is preview page
+      </React.Fragment>
+    )
+  }
+}
