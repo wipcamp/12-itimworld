9b82a76191e424d7394915662827e155c7f7af95
Unknows <firstpan555@gmail.com>
2019-12-22 22:47:22 +0700

    [UPDATE]Change Path get medthod in ProfileService.js

--- src/services/ProfileService.js
+++ src/services/ProfileService.js
@@ -2,7 +2,7 @@ import apiUserService from '../utils/apiUserService'
 
 const profileService = {
   getProfile: async (id) => {
-    let res = await apiUserService.get(`http://localhost:7999/user/${id}`)
+    let res = await apiUserService.get(`/user/${id}`)
     return res;
   },
   putProfile: async (data) => {
