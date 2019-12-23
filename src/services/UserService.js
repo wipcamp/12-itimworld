5e588fc0ca23730f00c7728e6cf8b795d4ec9e03
Unknows <firstpan555@gmail.com>
2019-12-23 15:04:22 +0700

    [UPDATE]Rename Profileservice to UserService

---
+++ src/services/UserService.js
@@ -0,0 +1,14 @@
+import apiUserService from '../utils/apiUserService'
+
+const userService = {
+  getUser: async (id) => {
+    let res = await apiUserService.get(`/user/${id}`)
+    return res;
+  },
+  putUser: async (data) => {
+    let res = await apiUserService.put(`/user/1`,data)
+    return res
+  }
+}
+
+export default userService
