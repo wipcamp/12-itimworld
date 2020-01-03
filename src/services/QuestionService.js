586fb2973fdbf0893102b6dfff20e370df3bf980
Nope <o_o.toto@windowslive.com>
2020-01-03 21:01:13 +0700

    [UPDATE] remove getQuestion ,use getMajorById then QuestionList instead

--- src/services/QuestionService.js
+++ src/services/QuestionService.js
@@ -1,10 +1,7 @@
 import apiUserService from '../utils/apiUserService'
 
 const questionService = {
-  getQuestion: async (majorId) => {
-    let res = await apiUserService.get(`http://localhost:8080/question/major/${majorId}`)
-    return res;
-  },
+  
 }
 
 export default questionService
