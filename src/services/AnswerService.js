ddd572a1d1a0998d2082c7b736c49be72ed2b2f0
Nope <o_o.toto@windowslive.com>
2020-01-03 21:00:06 +0700

    [UPDATE] remove localhost path from AnswerService

--- src/services/AnswerService.js
+++ src/services/AnswerService.js
@@ -2,11 +2,11 @@ import apiUserService from '../utils/apiUserService'
 
 const answerService = {
   getAnswer: async (userId,majorId) => {
-    let res = await apiUserService.get(`http://localhost:8080/user/${userId}/major/${majorId}/answer`)
+    let res = await apiUserService.get(`user/${userId}/major/${majorId}/answer`)
     return res;
   },
   postAnswer: async (userId,majorId,data) => {
-    let res = await apiUserService.post(`http://localhost:8080/user/${userId}/major/${majorId}/answer`,data)
+    let res = await apiUserService.post(`user/${userId}/major/${majorId}/answer`,data)
     return res;
   },
 }
