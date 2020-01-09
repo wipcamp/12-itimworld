1f9f0ac0d62c38db5304fc40823ffbc39a717439
Unknows <firstpan555@gmail.com>
2020-01-09 14:38:11 +0700

    [ADD]Modal in Edit

---
+++ src/components/Edit/Modal.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export default class Modal extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#staticBackdrop" disabled={this.props.disabled}>บันทึก</button>
+
+        <div class="modal fade" id="staticBackdrop" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                ...
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Understood</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    )
+  }
+}
