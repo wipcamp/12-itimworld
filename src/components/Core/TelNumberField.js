ee2a0534c0bbbe7e397cd6571da66a5722c2d112
pipe34150 <pipe34150@hotmail.com>
2020-01-10 19:03:56 +0700

    [UPDATE] Responsive on telnumberfiled.js

--- src/components/Core/TelNumberField.js
+++ src/components/Core/TelNumberField.js
@@ -3,19 +3,24 @@ import PropTypes from 'prop-types'
 
 const TelNumberField = (props) => {
   return (
-    <label id="telephone">
-      {props.labelInput}
-      <input
-        type="tel" 
-        id={props.name} 
-        name={props.name} 
-        placeholder="0800000000" 
-        value={props.value}
-        pattern="[0-9]{10}" 
-        maxLength="10" 
-        onChange={props.onChange}
-        required 
-        /> 
+    <label className="col-12 col-md-6 form-group" id="telephone">
+      <div className="row">
+        <div className="col-12 col-md-4 col-form-label text-md-right">{props.labelInput}</div>
+        <div className="col-12 col-md-8">
+          <input
+            className="form-control"
+            type="tel" 
+            id={props.name} 
+            name={props.name} 
+            placeholder="0800000000" 
+            value={props.value}
+            pattern="[0-9]{10}" 
+            maxLength="10" 
+            onChange={props.onChange}
+            required 
+            /> 
+        </div>
+      </div>
     </label>
   )
 }
