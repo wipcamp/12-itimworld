52efd51b6458e50a1b58e92688ad1cc932ed721d
Unknows <firstpan555@gmail.com>
2020-01-11 13:21:21 +0700

    [ADD]ButtonStyle in Core

--- src/components/Core/ButtonStyle.js
+++ src/components/Core/ButtonStyle.js
@@ -1,19 +1,17 @@
 import styled from 'styled-components'
+import { Button } from 'reactstrap';
 
-export const ButtonStyle = styled.button`
-  width: 218.65px;
-  height: 59px;
-
-  background: #304151;
-  border-radius: 4px;
-
-  font-family: Sarabun;
-  font-style: normal;
-  font-weight: 300;
-  font-size: 16px;
-  line-height: 21px;
-  align-items: center;
-  text-align: center;
-
+export const ButtonStyle = styled(Button)`
+  width: 218.65px!important;
+  height: 59px!important;
+  background: #304151!important;
+  border-radius: 4px!important;
+  font-style: normal!important;
+  font-weight: 300!important;
+  font-size: 16px!important;
+  line-height: 21px!important;
+  align-items: center!important;
+  text-align: center!important;
   color: #FFFFFF;
 `
+
