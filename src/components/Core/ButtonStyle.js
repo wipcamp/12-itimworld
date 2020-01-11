0234e7c42255fb61d82a21ada678d1c57d87a863
Unknows <firstpan555@gmail.com>
2020-01-11 10:33:31 +0700

    [ADD]ButtonStyle in Core

---
+++ src/components/Core/ButtonStyle.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const ButtonStyle = styled.button`
+  width: 218.65px;
+  height: 59px;
+
+  background: #304151;
+  border-radius: 4px;
+
+  font-family: Sarabun;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 16px;
+  line-height: 21px;
+  align-items: center;
+  text-align: center;
+
+  color: #FFFFFF;
+`
