e8d3e15a6d51bb3d966d700895ba44488e0e4686
Unknows <firstpan555@gmail.com>
2020-01-13 23:46:06 +0700

    [UPDATE]Text Style Header and Subtitle in Text.js

---
+++ src/components/Core/Text.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const HeaderText = styled.h1`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 36px;
+  line-height: 47px;
+  text-align: center!important;
+`
+
+export const Subtitle = styled.div`
+  font-style: normal;
+  font-weight: 300;
+  font-size: 16px;
+  line-height: 21px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: #000000;
+  margin-bottom: 3em;
+`
