5f81cc0dab91c5862256e3b30a2bf84a1d1f8ebc
Nope <o_o.toto@windowslive.com>
2020-01-15 18:22:27 +0700

    [ADD] add preview modal for confirm

---
+++ src/components/Preview/ConfirmModal.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components'
+import { Modal, ModalBody } from 'reactstrap';
+
+import ButtonRoute from '../Core/ButtonRoute'
+import { ButtonStyle } from '../Core/ButtonStyle'
+import { HeaderText, Paragrph } from '../Core/Text'
+
+const Hr = styled.hr`
+  width: 557px;
+  background-color:#3C3C3C;
+  border: 1px solid #3C3C3C;
+`
+const Div = styled.div`
+  margin-bottom: 2em;
+`
+const Body = styled(ModalBody)`
+  margin-left: -7.75em;
+  width: 745px;
+  height: 401px;
+  background: #EFEFEF;
+`
+const Alret = styled.div`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 23px;
+  text-align: center;
+  color: #FF0000;
+  margin-bottom: 3em;
+`
+
+const ConfirmModal = (props) => {
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
+  return (
+    <React.Fragment>
+      <div className={props.className}>
+        <ButtonStyle onClick={toggle} disabled={props.disabled}>ถัดไป</ButtonStyle>
+      </div>
+      <Modal isOpen={modal} toggle={toggle} className="d-flex justify-content-center">
+        <Body>
+          <HeaderText className="mt-3">ยืนยันที่จะส่งข้อมูล</HeaderText>
+          <Hr/>
+          <Paragrph className="row justify-content-center">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five 
+          </Paragrph>
+        <Alret> *หากยืนยันข้อมูล จะไม่สามารถกลับมาเเก้ไขส่วนคำถามที่ตอบเเละข้อมูลสาขาได้อีก </Alret>
+        <Div className="row justify-content-around ml-5 pl-4 mr-auto pr-auto">
+          <ButtonStyle onClick={toggle}>Cancel</ButtonStyle>
+          <ButtonRoute className="d-flex col-6" linkNext={`/success`} buttonRight="ยืนยัน" displayButtonLeft="none" />
+        </Div>
+        </Body>
+      </Modal>
+    </React.Fragment>
+  );
+}
+
+ConfirmModal.propTypes = {
+  className: PropTypes.string
+}
+
+ConfirmModal.defaultProps = {
+  className: 'col-6 d-inline-flex justify-content-end'
+}
+
+export default ConfirmModal;
