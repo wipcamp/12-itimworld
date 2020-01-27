f5d8cf184d69796376f5124afeec8ed3286b7584
Unknows <firstpan555@gmail.com>
2020-01-27 17:16:49 +0700

    [UPDATE]Auto Close reginter

--- src/components/Core/index.js
+++ src/components/Core/index.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
+import dayjs from 'dayjs'
 
 import Routing from '../../Router'
+
+const dateNow = dayjs().format('YYYY-MM-DDTHH:mm')
+// const dateNow = dayjs('2020-03-14T00:00')
+const dateEnd = dayjs('2020-03-13T00:00')
 
 export default class Index extends Component {
   render() {
     return (
-      <Routing />
+      <React.Fragment>
+        {
+          dateEnd.isAfter(dateNow)
+          ?
+          <Routing />
+          :
+          'ปิดรับสมัครแล้วจ้า'
+        }
+      </React.Fragment>
     )
   }
 }
