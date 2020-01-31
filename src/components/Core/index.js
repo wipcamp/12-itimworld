d4610f251a137ecd8afee0083e5acc9bf5dd5567
pipe34150 <pipe34150@hotmail.com>
2020-01-31 16:45:26 +0700

    [UPDATE] add background color in core/index

--- src/components/Core/index.js
+++ src/components/Core/index.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 
 import Routing from '../../Router'
 
+const ThisIsBackground = styled.div`
+  min-height: 100vh;
+  background: linear-gradient(180deg, #0F0C29 0%, rgba(2, 19, 91, 0.552083) 75%, #FFFFFF 100%);
+`
 export default class Index extends Component {
   componentDidUpdate() {
     // if (fakeAuth.isAuthenticated) {
@@ -12,7 +17,9 @@ export default class Index extends Component {
   }
   render() {
     return (
+      <ThisIsBackground>
         <Routing />
+      </ThisIsBackground>
     )
   }
 }
