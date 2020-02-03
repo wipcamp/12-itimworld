da96043973677f5e39aeb7241bde549d4f0d8652
pipe34150 <pipe34150@hotmail.com>
2020-02-03 13:34:30 +0700

    [UPDATE] add mountain into Core/index.js

--- src/components/Core/index.js
+++ src/components/Core/index.js
@@ -10,7 +10,18 @@ const dateEnd = dayjs('2020-03-13T00:00')
 
 const ThisIsBackground = styled.div`
   min-height: 100vh;
+  height:100%;
   background: linear-gradient(180deg, #0F0C29 0%, rgba(2, 19, 91, 0.552083) 75%, #FFFFFF 100%);
+  z-index:-2;
+`
+
+const Mountain = styled.div`
+  background-image:url('/img/mountain.png');
+  background-repeat: no-repeat;
+  background-position-y: bottom;
+  background-size:contain;
+  min-height: 100vh;
+  width: 100%;
 `
 export default class Index extends Component {
   componentDidUpdate() {
@@ -27,7 +38,9 @@ export default class Index extends Component {
           dateEnd.isAfter(dateNow)
           ?
             <ThisIsBackground>
+              <Mountain>
               <Routing />
+              </Mountain>
             </ThisIsBackground>
           :
           'ปิดรับสมัครแล้วจ้า'
