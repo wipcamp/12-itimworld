8ad5119b1ef5a5aaab40644539196c99806df07e
Unknows <firstpan555@gmail.com>
2020-02-05 18:12:48 +0700

    [UPDATE]Add url param in LineService

--- src/services/LineService.js
+++ src/services/LineService.js
@@ -2,8 +2,8 @@ import api from '../utils/apiLineService'
 // import Cookies from './CookieService'
 
 const lineService = {
-  lineLogin: async (code, nonce) => {
-    let res = await api.get(`/userAuth?code=${code}&nonce=${nonce}`)
+  lineLogin: async (code, nonce, url) => {
+    let res = await api.get(`/userAuth?code=${code}&nonce=${nonce}&url=${url}`)
     return res;
   },
   getGenerateCode: async () => {
