0c81b7f949f14cab8a55b444dbb2cc3b09ca5860
Unknows <firstpan555@gmail.com>
2020-02-11 17:25:37 +0700

    [UPDATE]Add AuthContext in Router.js

--- src/Router.js
+++ src/Router.js
@@ -25,25 +25,30 @@ const cookies = new Cookies()
 
 const PrivateRoute = ({ children, ...rest }, props) => {
   return (
-    <React.Fragment>
-      <Route
-        {...rest}
-        render={({ location }) =>
-          !(cookies.get('token') !== undefined || cookies.get('token') !== null) ? (
-            children
-          ) : (
-              <Redirect
-                to={{
-                  pathname: "/login",
-                  state: { from: location }
-                }}
-              />
-            )
-        }
-      />
-      {console.log(props.isAuthenticated)}
-    </React.Fragment>
-  )
+    <Authentication.Consumer>
+      {
+        ({ isAuthenticated }) => (
+          <React.Fragment>
+            <Route
+              {...rest}
+              render={({ location }) =>
+                isAuthenticated ? (
+                  children
+                ) : (
+                    <Redirect
+                      to={{
+                        pathname: "/login",
+                        state: { from: location }
+                      }}
+                    />
+                  )
+              }
+            />
+          </React.Fragment>
+        )
+      }
+    </Authentication.Consumer>
+  );
 }
 
 export default class Index extends React.Component {
@@ -53,55 +58,64 @@ export default class Index extends React.Component {
     user: false,
     isAuthenticated: false
   }
-  
-  componentDidMount(){
-    if (cookies.get('token') !== undefined && cookies.get('token') !== null && !this.state.isAuthenticated ){
+
+  componentDidMount() {
+    if (cookies.get('token') !== undefined && cookies.get('token') !== null && !this.state.isAuthenticated) {
       console.log('token')
       console.log(cookies.get('token'))
-      this.setState({
-        isAuthenticated: true
-      })
+      this.changeAuthen()
     }
+  }
+
+  changeAuthen = () => {
+    this.setState({
+      isAuthenticated: true
+    })
   }
 
   render() {
     return (
-      <Router>
-        <Navbar isAuthenticated={this.state.isAuthenticated} />
-        <Switch>
-          <Route path="/login" >
-            <Login />
-          </Route>
-          <PrivateRoute path="/profile" isAuthenticated={this.state.isAuthenticated}>
-            <Profile />
-          </PrivateRoute>
-          <PrivateRoute path="/general" isAuthenticated={this.state.isAuthenticated}>
-            <General />
-          </PrivateRoute>
-          <PrivateRoute path="/major" isAuthenticated={this.state.isAuthenticated}>
-            <Major />
-          </PrivateRoute>
-          <PrivateRoute path="/menu" isAuthenticated={this.state.isAuthenticated}>
-            <Menu />
-          </PrivateRoute>
-          <PrivateRoute path="/document" isAuthenticated={this.state.isAuthenticated}>
-            {/* <Menu /> */}
-          </PrivateRoute>
-          <PrivateRoute path="/questions" isAuthenticated={this.state.isAuthenticated}>
-            <Questions />
-          </PrivateRoute>
-          <PrivateRoute path="/preview" isAuthenticated={this.state.isAuthenticated}>
-            <Preview />
-          </PrivateRoute>
-          <PrivateRoute path="/success" isAuthenticated={this.state.isAuthenticated}>
-            <Success />
-          </PrivateRoute>
-          <PrivateRoute path="/edit" isAuthenticated={this.state.isAuthenticated}>
-            <Edit />
-          </PrivateRoute>
-        <PrivateRoute path="*" isAuthenticated={this.state.isAuthenticated} />
-        </Switch>
-      </Router>
+      <Authentication.Provider value={{
+        isAuthenticated: this.state.isAuthenticated,
+        changeAuthen: this.changeAuthen
+      }}>
+        <Router>
+          <Navbar isAuthenticated={this.state.isAuthenticated} />
+          <Switch>
+            <Route path="/login" >
+              <Login />
+            </Route>
+            <PrivateRoute path="/profile" isAuthenticated={this.state.isAuthenticated}>
+              <Profile />
+            </PrivateRoute>
+            <PrivateRoute path="/general" isAuthenticated={this.state.isAuthenticated}>
+              <General />
+            </PrivateRoute>
+            <PrivateRoute path="/major" isAuthenticated={this.state.isAuthenticated}>
+              <Major />
+            </PrivateRoute>
+            <PrivateRoute path="/menu" isAuthenticated={this.state.isAuthenticated}>
+              <Menu />
+            </PrivateRoute>
+            <PrivateRoute path="/document" isAuthenticated={this.state.isAuthenticated}>
+              {/* <Menu /> */}
+            </PrivateRoute>
+            <PrivateRoute path="/questions" isAuthenticated={this.state.isAuthenticated}>
+              <Questions />
+            </PrivateRoute>
+            <PrivateRoute path="/preview" isAuthenticated={this.state.isAuthenticated}>
+              <Preview />
+            </PrivateRoute>
+            <PrivateRoute path="/success" isAuthenticated={this.state.isAuthenticated}>
+              <Success />
+            </PrivateRoute>
+            <PrivateRoute path="/edit" isAuthenticated={this.state.isAuthenticated}>
+              <Edit />
+            </PrivateRoute>
+            <PrivateRoute path="*" isAuthenticated={this.state.isAuthenticated} />
+          </Switch>
+        </Router>
+      </Authentication.Provider>
     )
   }
 }
