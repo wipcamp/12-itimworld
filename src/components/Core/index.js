00af3409271d784440ce4c453a28ad82a6b7d496
Unknows <firstpan555@gmail.com>
2020-02-12 18:18:57 +0700

    [UPDATE]Remove cookie token when close tab

--- src/components/Core/index.js
+++ src/components/Core/index.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react'
 import dayjs from 'dayjs'
+import { Beforeunload } from 'react-beforeunload';
+import Cookies from 'universal-cookie'
 
 import Routing from '../../Router'
+
+const cookies = new Cookies()
 
 const dateNow = dayjs().format('YYYY-MM-DDTHH:mm')
 // const dateNow = dayjs('2020-03-14T00:00')
 const dateEnd = dayjs('2020-03-13T00:00')
 
 export default class Index extends Component {
-  componentDidUpdate() {
-    // if (fakeAuth.isAuthenticated) {
-    //   this.changeAuthen()
-    // }
-    console.log(2)
-    // console.log(fakeAuth.isAuthenticated)
-  }
+
   render() {
     return (
       <React.Fragment>
         {
           dateEnd.isAfter(dateNow)
           ?
-          <Routing />
-          :
-          'ปิดรับสมัครแล้วจ้า'
-        }
+            <Beforeunload onBeforeunload={() => cookies.remove('token', { path: '/' }) } >
+              <Routing />
+            </Beforeunload>
+            :
+            'ปิดรับสมัครแล้วจ้า'
+          }
       </React.Fragment>
     )
   }
