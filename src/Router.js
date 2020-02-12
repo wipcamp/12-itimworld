98e4b36bf2394d28f6e91e347d24ce76718d41e3
Unknows <firstpan555@gmail.com>
2020-02-12 17:11:00 +0700

    [UPDATE]change condition Auth

--- src/Router.js
+++ src/Router.js
@@ -29,7 +29,7 @@ const PrivateRoute = ({ children, ...rest }) => {
       <Route
         {...rest}
         render={({ location }) =>
-          (cookies.get('token') !== undefined || cookies.get('token') !== null)? (
+          (cookies.get('token') !== undefined && cookies.get('token') !== null)? (
             children
           ) : (
               <Redirect
@@ -41,7 +41,7 @@ const PrivateRoute = ({ children, ...rest }) => {
             )
         }
       />
-      {console.log("Auth" + (cookies.get('token') !== undefined || cookies.get('token') !== null))}
+      {console.log("Auth" + (cookies.get('token') !== undefined && cookies.get('token') !== null))}
     </React.Fragment>
   )
 }
@@ -72,34 +72,34 @@ export default class Index extends React.Component {
           <Route path="/login" >
             <Login />
           </Route>
-          <PrivateRoute path="/profile" isAuthenticated={this.state.isAuthenticated}>
+          <PrivateRoute path="/profile">
             <Profile />
           </PrivateRoute>
-          <PrivateRoute path="/general" isAuthenticated={this.state.isAuthenticated}>
+          <PrivateRoute path="/general">
             <General />
           </PrivateRoute>
-          <PrivateRoute path="/major" isAuthenticated={this.state.isAuthenticated}>
+          <PrivateRoute path="/major">
             <Major />
           </PrivateRoute>
-          <PrivateRoute path="/menu" isAuthenticated={this.state.isAuthenticated}>
+          <PrivateRoute path="/menu">
             <Menu />
           </PrivateRoute>
-          <PrivateRoute path="/document" isAuthenticated={this.state.isAuthenticated}>
+          <PrivateRoute path="/document">
             {/* <Menu /> */}
           </PrivateRoute>
-          <PrivateRoute path="/questions" isAuthenticated={this.state.isAuthenticated}>
+          <PrivateRoute path="/questions">
             <Questions />
           </PrivateRoute>
-          <PrivateRoute path="/preview" isAuthenticated={this.state.isAuthenticated}>
+          <PrivateRoute path="/preview">
             <Preview />
           </PrivateRoute>
-          <PrivateRoute path="/success" isAuthenticated={this.state.isAuthenticated}>
+          <PrivateRoute path="/success">
             <Success />
           </PrivateRoute>
-          <PrivateRoute path="/edit" isAuthenticated={this.state.isAuthenticated}>
+          <PrivateRoute path="/edit">
             <Edit />
           </PrivateRoute>
-        <PrivateRoute path="*" isAuthenticated={this.state.isAuthenticated} />
+        <PrivateRoute path="*" />
         </Switch>
       </Router>
     )
