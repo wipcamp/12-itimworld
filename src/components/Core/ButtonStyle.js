719b4e4052de0aa2f3aecdaf88389872f762fdde
pipe34150 <pipe34150@hotmail.com>
2020-02-14 14:45:22 +0700

    [UPDATE] ButtonStyle.js with responsive size

--- src/components/Core/ButtonStyle.js
+++ src/components/Core/ButtonStyle.js
@@ -14,6 +14,24 @@ export const ButtonStyle = styled(Button)`
   align-items: center!important;
   text-align: center!important;
   color: #FFFFFF;
+
+  @media (max-width: 768px) {
+    width: 150.65px!important;
+    height: 49px!important;
+    border-radius: 4px!important;
+    font-weight: 300!important;
+    font-size: 16px!important;
+    line-height: 21px!important;
+  }
+
+  @media (max-width: 576px) {
+    width: 88.8px!important;
+    height: 33.76px!important;
+    border-radius: 4px!important;
+    font-weight: 300!important;
+    font-size: 16px!important;
+    line-height: 10px!important;
+  }
 `
 
 export const ButtonStyleLink = styled(Link)`
@@ -32,6 +50,24 @@ export const ButtonStyleLink = styled(Link)`
   &:hover{
     color: #FFFFFF!important;
     text-decoration:none;
+  }
+
+  @media (max-width: 768px) {
+    width: 150.65px!important;
+    height: 49px!important;
+    border-radius: 4px!important;
+    font-weight: 300!important;
+    font-size: 16px!important;
+    line-height: 21px!important;
+  }
+
+  @media (max-width: 576px) {
+    width: 88.8px!important;
+    height: 33.76px!important;
+    border-radius: 4px!important;
+    font-weight: 300!important;
+    font-size: 16px!important;
+    line-height: 10px!important;
   }
 `
 
