1e13041a0f47c73aeec58ec6ce1a107950fc5870
Nope <o_o.toto@windowslive.com>
2020-02-17 18:35:12 +0700

    [UPDATE] make Question page work correctly

--- src/components/Questions/index.js
+++ src/components/Questions/index.js
@@ -4,22 +4,25 @@ import Question from './Question'
 import MajorService from './../../services/MajorService'
 import AnswerService from './../../services/AnswerService'
 import UserService from './../../services/UserService'
-import Styled from 'styled-components'
+import styled from 'styled-components'
+import {HeaderText} from './../Core/Text'
+import {ButtonStyle} from './../Core/ButtonStyle'
+import CustomModal from './../Core/CustomModal'
+import {Redirect} from 'react-router-dom'
 
 let answer = [];
 let majorId = 1;
 let userId = 120001;
 
-const Header = Styled.h2`
-font-family: Sarabun;
-font-style: normal;
-font-weight: bold;
-font-size: 36px;
-line-height: 47px;
-text-align: center;
+const ContainerDiv = styled.div`
+  max-width:1200px;
 `
 
+const NotDisplayButton = styled.button`
+  display:none;
+`
 
+let submitButtonRef = null;
 export default class Index extends Component {
   
   handleAnswer=(event)=> {
@@ -45,24 +48,25 @@ export default class Index extends Component {
   questions = [];
   
     state = {
-      finishLoad:false,
-      errorLoad:false, 
-        questions : 
-          [
-            { 
-              id:1,
-              name:'Mock1'
-            },
-            {
-              id:2,
-              name:'Mock2'
-            },
-            {
-              id:3,
-              name:'Mock3'
-            },
-          ]
-        }
+      finishLoad: false,
+      errorLoad: false,
+      questions: [{
+          id: 1,
+          name: 'Mock1'
+        },
+        {
+          id: 2,
+          name: 'Mock2'
+        },
+        {
+          id: 3,
+          name: 'Mock3'
+        },
+      ],
+      confirmModal:false,
+      alertModal:false,
+      redirect:false
+    }
 
     async componentDidMount() {
       let search = window.location.search;
@@ -70,6 +74,29 @@ export default class Index extends Component {
       majorId = params.get('major');
       
       await this.getQuestionService(majorId);
+      console.log(submitButtonRef);
+      
+    }
+
+    setSubmitButtonRef = (e) => {
+      submitButtonRef = e;
+    }
+
+    clickSubmit = () => {
+      submitButtonRef.click();
+    }
+
+    toggleConfirmModal = () => {
+      this.setState({confirmModal: !this.state.confirmModal})
+    }
+
+    toggleAlertModal = () => {
+      this.setState({alertModal: !this.state.alertModal})
+    }
+
+    openConfirmModal = e => {
+      e.preventDefault();
+      this.toggleConfirmModal();
     }
 
       getQuestionService = async (majorId) => {
@@ -93,11 +120,20 @@ export default class Index extends Component {
       }
     
     postAnswerService = async() =>{
-      let response = await AnswerService.postAnswer(userId,majorId,{"answers":answer}).then(() => {UserService.postStatus(userId,{"status":"major"})});
-        console.log(response);
+      await AnswerService.postAnswer(userId,majorId,{"answers":answer})
+        .then(() => {UserService.postStatus(userId,{"status":"major"})})
+        .then(() => this.setState({redirect:true}))
+        .catch(() => this.toggleAlertModal());
     }
     
     render() {
+
+      const { redirect } = this.state;
+
+      if (redirect) {
+        return <Redirect to='/menu'/>;
+      }
+
       if(!this.state.finishLoad || this.state.errorLoad){
         if(this.state.errorLoad){
           return <p>Error Load</p>
@@ -105,26 +141,49 @@ export default class Index extends Component {
         return <p>Loading...</p>
       }else{
         return (
-          <div className="container bg-white"><br/>
-            <Header classname="col-12 mb-5 mt-5 ">คำถามสาขา</Header>
-              <div>
-                      {this.state.questions.map((data,i) => {
-                        return <Question
-                        questionCount={i+1}
-                        questionName={data.name}
-                        questionId={data.id}
-                        handleAnswer={this.handleAnswer}
-                        />
-                      })}
+          <ContainerDiv className ="container-fluid justify-content-center">
+            <div className="card p-5" style={{boxShadow: `0px 4px 4px rgba(0, 0, 0, 0.25)`,borderRadius: `4px`,opacity:`0.9`}}>
+              <form onSubmit={e=>this.openConfirmModal(e)}>
+                <HeaderText classname="col-12 mb-5 mt-5">คำถามสาขา</HeaderText>
+                {this.state.questions.map((data,i) => {
+                  return <Question
+                    questionCount={i+1}
+                    questionName={data.name}
+                    questionId={data.id}
+                    handleAnswer={this.handleAnswer}
+                    required
+                  />
+                  })}
+                <NotDisplayButton ref={this.setSubmitButtonRef} />
+              </form>
+              <div class="row mt-5">
+                <ButtonRoute 
+                  buttonLeft="กลับ"  
+                  linkBack ="/major"
+                  className="col-6 d-inline-flex"
+                  displayButtonRight="none"
+                />
+                <div className="col-6 d-inline-flex justify-content-end">
+                  <ButtonStyle className="text-center" onClick={() => this.clickSubmit()}>ยืนยัน</ButtonStyle>
+                </div>
               </div>
-              <ButtonRoute 
-                buttonLeft="กลับ" 
-                buttonRight="ยืนยัน" 
-                linkBack ="/major"
-                linkNext ="/menu"
-                onClick={this.postAnswerService}
-              />
-          </div>
+            </div>
+            <CustomModal 
+              modal={this.state.confirmModal}
+              toggle={this.toggleConfirmModal}
+              header="ยืนยันที่จะเลือกสาขา" 
+              paragraph="Lorem Ipsum"
+              dangerSubtitle="*เมื่อเลือกสาขาแล้ว ไม่สามารถย้อนกลับมา" 
+              primaryButtonDisplay="flex"
+              primaryOnClick={() => this.postAnswerService()}
+            />
+            <CustomModal 
+              modal={this.state.alertModal}
+              toggle={this.toggleAlertModal}
+              header="เกิดข้อผิดพลาดขึ้น" 
+              paragraph="โปรดติดต่อเจ้าหน้าที่"
+            />
+          </ContainerDiv>
         )
       }
     }
