c741bc31c917703f66ff546f3dd18553da3ee30f
Unknows <firstpan555@gmail.com>
2020-02-17 17:07:47 +0700

    [UPDATE]Line CI Login

--- src/components/Login/LineLoginButton.js
+++ src/components/Login/LineLoginButton.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
 
 const ButtonContainer = styled.div`
   width: 100%;
@@ -10,74 +9,66 @@ const ButtonContainer = styled.div`
   max-width: 400px;
   height: 70px;
 `
-const Button = styled.div`
+const Button = styled.button`
   height: 70px;
   width: 100%;
   max-width: 400px;
   border-radius: 20px;
-  box-shadow: 0 1px 2px rgba(0,0,0,0.15);
-  transition: all 0.3s ease-in-out;
-  @media(max-width: 500px) {
-    max-width: 350px;
-  }
-  @media(max-width: 360px) {
-    max-width: 300px;
-  }
-  &::after {
-  content: '';
-  position: absolute;
-  z-index: -1;
-  height: 70px;
-  max-width: 400px;
-  @media(max-width: 500px) {
-    max-width: 350px;
-  }
-  @media(max-width: 360px) {
-    max-width: 300px;
-  }
-  width: 100%;
-  opacity: 0;
+  padding:0!important;
+  &:active{
   border-radius: 20px;
-  box-shadow: 0 5px 15px rgba(0,0,0,0.3);
-  transition: opacity 0.3s ease-in-out;
-  }
-  &:hover {
-  transform: scale(1.2, 1.2);
-  }
-  &:hover::after {
-  opacity: 1;
   }
 `
 
 const LineImg = styled.img`
   height:100%;
+  content:url('${props => props.imgLine}');
 `
 
 const Text = styled.div`
-  background: #00C300;
+  background: ${props => props.bgColor};
   color: #FFFFFF;
   border-radius: 20px;
   height:100%;
   font-size: 24px;
   width:100%;
   padding-right: 3rem;
-  @media(max-width: 500px) {
-    font-size: 20px;
-  }
-  @media(max-width: 360px) {
-    font-size: 15px;
-    padding-right: 1rem;
-  }
 `
 
 export default class LineLoginButton extends Component {
+  state = {
+    imgLine: '/img/btn_base.png',
+    bgColor: '#00C300'
+  }
+
+  mouseOver = (e) => {
+    this.setState({
+      imgLine: '/img/btn_hover.png',
+      bgColor: '#00E000'
+    })
+  }
+
+  mouseOut = (e) => {
+    this.setState({
+      imgLine: '/img/btn_base.png',
+      bgColor: '#00C300'
+    })
+  }
+
+  mouseClick = () => {
+    this.props.onClick()
+    this.setState({
+      imgLine: '/img/btn_press.png',
+      bgColor:'#00b300'
+    })
+  }
 
   render() {
     return (
-      <ButtonContainer className="mt-5">
-        <Button onClick={this.props.onClick} className="row" >
-          <Text className="d-flex align-items-center justify-content-between pl-2">
-            <LineImg src="/img/btn_base.png" />
+      <ButtonContainer className="mt-5" >
+        <Button onClick={() => this.mouseClick()} onMouseOver={() => this.mouseOver()} onMouseOut={() => this.mouseOut()} className="btn row" >
+          <Text className="d-flex align-items-center justify-content-between pl-2" bgColor={this.state.bgColor}>
+            <LineImg imgLine={this.state.imgLine}/>
             <div className="col-8">เข้าสู่ระบบด้วย Line</div>
           </Text>
         </Button>
