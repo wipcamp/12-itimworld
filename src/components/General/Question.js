912334cefb3359965461785b60bfa70a5bd4ee4a
Nope <o_o.toto@windowslive.com>
2020-02-19 15:53:12 +0700

    [UPDATE] make General/Question can display audio media

--- src/components/General/Question.js
+++ src/components/General/Question.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import {Subtitle} from './../Core/Text'
 
 const QuestionName = styled.p`
     font-weight: 500;
@@ -18,10 +19,36 @@ const TextArea = styled.textarea`
     box-sizing: border-box;
     border-radius: 4px;
 `
+
+const Media = styled.audio`
+  width:300px;
+  @media (max-width: 576px) {
+    width: 200px!important;
+  }
+`
+
+const DangerSubtitle = styled(Subtitle)`
+  color:red;
+`
+
+const displayAppPlayer = (id) => {
+  if(id === 3){
+    return (
+      <div class="col-12 justify-content-start mb-2">
+         <Media controls>
+            <source src="/audio/WIPAudio1.m4a" type="audio/mp3" />
+            <DangerSubtitle>*Browser ของท่านไม่ Support Audio Player</DangerSubtitle>
+          </Media> 
+      </div>
+    )
+  }
+}
+
 const Question = (props) => {
   return (
     <div className="form-group">
       <QuestionName className="col-12 justify-content-center">คำถามที่ {props.questionCount} : {props.questionName}</QuestionName>
+      {displayAppPlayer(props.questionId)}
       <TextArea 
         className="col-12"
         name={props.questionId} 
