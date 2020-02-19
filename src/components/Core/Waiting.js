253592fc48f3a03de4a98d0aa618b611ad51dc3e
pipe34150 <pipe34150@hotmail.com>
2020-02-19 19:15:04 +0700

    [UPDATE] Loading Image with rotate

--- src/components/Core/Waiting.js
+++ src/components/Core/Waiting.js
@@ -11,8 +11,54 @@ const Error = () => {
 }
 
 const Loading = () => {
-  return <ReactLoading type="spinningBubbles" color="#FFFFFF" />;
+  return <div className="">
+      <Sky>
+        <Skypic src='img/Loading/l_bge.png' />
+        <Wippo src='/img/Loading/l_maine.png' />
+      </Sky>
+    </div>;
 }
+
+const Sky = styled.div`
+  /* background-image:url('/img/Loading/l_bge.png'); */
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: auto;
+`
+
+const Wippo = styled.img`
+  position: absolute;
+  margin: auto;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 10vh;
+  width: 20%;
+  @media (max-width: 992px) {
+    width:30%;
+  }
+
+  @media (max-width: 768px) {
+    width:40%;
+  }
+
+  @media (max-width: 576px) {
+    width:80%;
+  }
+`
+
+const Skypic = styled(Wippo)`
+  animation: rotate 15s linear infinite;
+
+  @keyframes rotate {
+    from{
+        transform: rotate(0deg);
+    }
+    to{
+        transform: rotate(360deg);
+    }
+  }
+`
 
 const display = (error) => {
   if(error)
@@ -24,9 +70,9 @@ const display = (error) => {
 const Landing = (props) => {
   return (
     <div class="container">
-      <MiddleScreen className="row justify-content-center">
+      <div className="row text-center">
         {display(props.error)}
-      </MiddleScreen>
+      </div>
     </div>
     )
   };
