2e6b606d1b65c56bddc8a77b42b66f9fb65bcc4a
First <32408003+First-unknow@users.noreply.github.com>
2020-02-20 21:22:03 +0700

    [UPDATE]Dev flow

--- src/Router.js
+++ src/Router.js
@@ -43,8 +43,8 @@ const PrivateRoute = ({ children, ...rest }) => {
       <Route
         {...rest}
         render={({ location }) =>
-          (cookies.get('token') !== undefined && cookies.get('token') !== null)? (
-          // true ? (
+//           (cookies.get('token') !== undefined && cookies.get('token') !== null)? (
+          true ? (
             <React.Fragment>
             {
                 locationNow === '/menu' || locationNow === '/profile' || 
@@ -78,8 +78,8 @@ const MenuRoute = () => {
   return(
     <React.Fragment>
       {
-        (cookies.get('token') !== undefined && cookies.get('token') !== null)? (
-        // true ? (
+//         (cookies.get('token') !== undefined && cookies.get('token') !== null)? (
+        true ? (
           <Menu />
         ) : (
             <Redirect
@@ -105,8 +105,8 @@ export default class Index extends React.Component {
     return (
       <Router>
         {
-          (cookies.get('token') !== undefined && cookies.get('token') !== null)  && locationNow !== '/login' ?
-          // true  && locationNow !== '/login' ?
+//           (cookies.get('token') !== undefined && cookies.get('token') !== null)  && locationNow !== '/login' ?
+          true  && locationNow !== '/login' ?
             <Navbar />:
             ''
         }
