78b0cbc203fd1048a26842d205547f0137653de9
Unknows <firstpan555@gmail.com>
2020-02-21 15:48:57 +0700

    [UPDATE] Redirect ath error in waiting

--- src/components/Core/Waiting.js
+++ src/components/Core/Waiting.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import styled from 'styled-components'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import { ButtonStyle , ButtonStyleLink } from './ButtonStyle'
 
 const Section = styled.div`
@@ -154,7 +160,12 @@ export const Error = () => {
 
 const display = (error) => {
   if(error)
-    return <Error />;
+    return <Redirect
+              to={{
+                pathname: "/error",
+                state: { from: locationNow }
+              }}
+            />;
   else
     return <Loading />;
 }
